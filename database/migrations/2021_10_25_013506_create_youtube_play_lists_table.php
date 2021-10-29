64f93361a71c101383f268f98ad5ef858225f6a8
dailyuno <34119537+dailyuno@users.noreply.github.com>
2021-10-29 13:09:54 +0900

    play list 국/영문 구분 컬럼 추가

--- database/migrations/2021_10_25_013506_create_youtube_play_lists_table.php
+++ database/migrations/2021_10_25_013506_create_youtube_play_lists_table.php
@@ -18,6 +18,7 @@ class CreateYoutubePlayListsTable extends Migration
             $table->string('title');
             $table->text('description')->nullable();
             $table->dateTimeTz('published_at')->nullable();
+            $table->enum('lang', ['kr', 'en'])->default('kr');
         });
     }
 
